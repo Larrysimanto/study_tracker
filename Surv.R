04569225836ab69d8ab301999b67793c355759a4
Larrysimanto <lawrencesimanto9@gmail.com>
2025-04-16 22:50:27 +0300

    Package install

---
+++ Surv.R
@@ -0,0 +1,7 @@
+#use scikit-learn xgboost lightgbm in conjunction with survivalist python package
+#for probabilistic survival analysis.
+
+utils::install.packages("reticulate")
+library(reticulate)
+
+#install necessary python packages
