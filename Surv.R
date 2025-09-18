31c032be61cc6bbb767e807d3c7d952e006581e8
knight <52457978+Larrysimanto@users.noreply.github.com>
2025-09-18 12:00:34 +0300

    Delete Surv.R

--- Surv.R
+++
@@ -1,7 +0,0 @@
-#use scikit-learn xgboost lightgbm in conjunction with survivalist python package
-#for probabilistic survival analysis.
-
-utils::install.packages("reticulate")
-library(reticulate)
-
-#install necessary python packages
